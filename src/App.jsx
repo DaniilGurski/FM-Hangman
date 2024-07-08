e5c8851322a4a46c7bb536ed6d5b3dec9f8c18c4
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2024-07-08 16:03:23 +0200

    init.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import "./scss/style.scss";
+
+function App() {
+  return (
+    <>
+      <p className="fs-tutorial-number">Lorem ipsum dolor.</p>
+    </>
+  )
+}
+
+export default App
