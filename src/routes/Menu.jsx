61dee4b3fcab60d9421bc6d9c858de4f910bb834
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2024-07-31 15:11:18 +0200

    h1.

--- src/routes/Menu.jsx
+++ src/routes/Menu.jsx
@@ -8,6 +8,7 @@ export default function Menu() {
         <div className="menu-panel action-panel"> 
             <div className="grid">
                 <img className="menu-panel__logo action-panel__header" src={logoUrl} alt="logo" />
+                <h1 className="visually-hidden"> The hangman game </h1> 
 
                 <LinkRoundButton className="round-button menu-panel__start-button" to="/categories" icon={iconPlay} ariaText={"start the game"}/>
 
